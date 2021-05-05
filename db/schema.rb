ccd9d822722a748917b730e1e8d6eb2908922722
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-05-05 20:15:04 -0300

    bookings model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_235255) do
+ActiveRecord::Schema.define(version: 2021_05_05_230444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "flat_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flat_id"], name: "index_bookings_on_flat_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "flats", force: :cascade do |t|
     t.string "title"
     t.string "address"
     t.string "city"
     t.text "description"
-    t.boolean "available"
+    t.boolean "available", default: true
     t.float "price"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -41,5 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_235255) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "flats"
+  add_foreign_key "bookings", "users"
   add_foreign_key "flats", "users"
 end
