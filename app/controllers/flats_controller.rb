6bb8c9992fcf543757a8d70fe3e21371665338e5
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-05-06 21:23:05 -0300

    created C, E, D and new view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,49 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
 
   def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    @flat.flat = Flat.find(params[:id])
+    if @flat.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to dashboard_path
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:title, :description, :address, :city, :price)
+  end
+
+  def set_flat
     @flat = Flat.find(params[:id])
   end
 end
