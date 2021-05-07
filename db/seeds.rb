665c56c0723667f71cfd60995a990860046884c2
felippe menezes <felippemenezes1982@gmail.com>
2021-05-06 22:38:17 -0300

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = []
+
+5.times do
+  user = User.new(
+    email:    Faker::Internet.email,
+    password: "abc123",
+    role: "traveler"
+  )
+  user.save!
+  users << user
+end
+
+10.times do
+  flat = Flat.new(
+    title:    Faker::Company.name,
+    address: Faker::Address.street_address,
+    city: Faker::Address.city,
+    description: Faker::Lorem.sentences(number: 1),
+    price: Faker::Number.decimal(l_digits: 2)
+  )
+
+  flat.user = users.sample
+
+  flat.save!
+end
+
+
