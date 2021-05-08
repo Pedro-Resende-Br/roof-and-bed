6e2099f98d264305ce6bafdcccdcece7fbe181e5
Pedro Resende Breitschaft <pedro_brei@hotmail.com>
2021-05-08 12:24:59 -0300

    Flat view html done, and altered the controller to redirect to infex while there is no dashboard

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,14 +10,15 @@ class FlatsController < ApplicationController
   end
 
   def new
-    @flat = Flat.new
+    @flat = Flat.new  
   end
 
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
     if @flat.save
-      redirect_to dashboard_path
+      #redirect_to dashboard_path
+      redirect_to flats_index_path
     else
       render :new
     end
