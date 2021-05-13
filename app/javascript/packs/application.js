3d6a4ef83d4dff479f1dec55741ff5224a22e925
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-05-13 20:46:51 -0300

    added stylesheet_pack_tag to application.html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import { initAutocomplete } from "../plugins/init_autocomplete"
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -17,12 +17,19 @@ const addMarkersToMap = (map, markers) => {
   });
 };
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+};
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
   if (mapElement) {
     const map = buildMap(mapElement);
     const markers = JSON.parse(mapElement.dataset.markers);
     addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
   }
 };
 
