ce5703f9cd6a276aeea2d2f781ef510554070a68
felippe menezes <felippemenezes1982@gmail.com>
2021-05-15 12:24:49 -0300

    Javascript banner fixed

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -4,7 +4,7 @@ const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Roof and Bed offers you the best flats!"],
     typeSpeed: 50,
-    loop: true
+    loop: false
   });
 }
 
