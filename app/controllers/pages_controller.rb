be3cb3448cc8d8f3353e0cd7e9b72f6ffb080ef5
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-07-06 19:56:01 -0300

    added order desc in dashboard bookings

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,6 @@ class PagesController < ApplicationController
   def dashboard
     user = current_user
     @flats = Flat.where("user_id = #{user.id}").paginate(page: params[:page], per_page: 9)
-    @bookings = Booking.where("user_id = #{user.id}")
+    @bookings = Booking.where("user_id = #{user.id}").order(created_at: :desc)
   end
 end
